6004f9e176acd1c8657a1b9f8d4c00c9d76a258f
Andy Speirs <Andy.Speirs@havas.com>
2018-03-10 15:12:13 +0000

    Versioned to 0.6 Updated image to work on blank and white watches Added .gitignore

--- src/c/main.c
+++ src/c/main.c
@@ -15,24 +15,15 @@ static GPath *s_minute_arrow, *s_hour_arrow;
 void getMonth(char *input, int location, struct tm *tick_time) {		// 0 is before, 1 is after
 	char month_current[16];
 	strftime(month_current, sizeof(month_current), "%B", tick_time);
- 	
-	#if defined(PBL_RECT)
-		strcat(input,"  %B");
-	#elif defined(PBL_ROUND)
-		if(strlen(month_current) > 5) {
-			strcat(input,"  %b");
-	 	} else {
-			strcat(input,"  %B");
-		}
-	#endif
+	strcat(input,"  %B");
 }
 
 void getSuffix(char *input, char date_current[5]) {
-	if (strncmp(date_current, "01", 2) == 0 || strncmp(date_current, "21", 2) == 0 || strncmp(date_current,"31",2) == 0) { 
+	if (strncmp(date_current, "01", 2) == 0 || strncmp(date_current, "21", 2) == 0 || strncmp(date_current,"31",2) == 0) {
 		strcat(input,"st");
 	} else if (strncmp(date_current, "02", 2) == 0 || strncmp(date_current, "22", 2) == 0) {
 		strcat(input,"nd");
-	} else if (strncmp(date_current, "03", 2) == 0 || strncmp(date_current, "23", 2) == 0) { 
+	} else if (strncmp(date_current, "03", 2) == 0 || strncmp(date_current, "23", 2) == 0) {
 		strcat(input,"rd");
 	} else {
 		strcat(input,"th");
@@ -45,7 +36,7 @@ void getSuffix(char *input, char date_current[5]) {
 
 void unobstructed_change(AnimationProgress progress, void* data) {
 	GRect b = layer_get_unobstructed_bounds(window_get_root_layer(s_window));
-	
+
 	GPoint center = grect_center_point(&b);
 	gpath_move_to(s_minute_arrow, center);
 	gpath_move_to(s_hour_arrow, center);
@@ -55,15 +46,15 @@ void unobstructed_change(AnimationProgress progress, void* data) {
 
 static void bluetooth_callback(bool connected) {
 	int colour_bluetooth = persist_read_int(MESSAGE_KEY_COLOUR_BLUETOOTH);
-	
+
 	if(!connected) {		// Disconected
 		if(colour_bluetooth) {
 			GColor bt_colour = GColorFromHEX(colour_bluetooth);
 			text_layer_set_text_color(s_weekday_label, bt_colour);
 			text_layer_set_text_color(s_month_label, bt_colour);
 		} else {
-			text_layer_set_text_color(s_weekday_label, GColorRed);
-			text_layer_set_text_color(s_month_label, GColorRed);
+			text_layer_set_text_color(s_weekday_label, PBL_IF_BW_ELSE(GColorWhite, GColorRed));
+			text_layer_set_text_color(s_month_label, PBL_IF_BW_ELSE(GColorWhite, GColorRed));
 		}
 		vibes_long_pulse();
 	} else {				// Connected
@@ -76,18 +67,18 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
 // Colours
 	Tuple *colour_bluetooth_t	= dict_find(iter, MESSAGE_KEY_COLOUR_BLUETOOTH);
 	int colour_bluetooth = colour_bluetooth_t->value->int32;
-	persist_write_int(MESSAGE_KEY_COLOUR_BLUETOOTH, colour_bluetooth);	
+	persist_write_int(MESSAGE_KEY_COLOUR_BLUETOOTH, colour_bluetooth);
 
 // Digital
 	Tuple *toggle_digital_t = dict_find(iter, MESSAGE_KEY_TOGGLE_DIGITAL);
 	int toggle_digital = toggle_digital_t->value->int32;
 	persist_write_int(MESSAGE_KEY_TOGGLE_DIGITAL, toggle_digital);
-	
-// Date	
+
+// Date
 	Tuple *toggle_suffix_t = dict_find(iter, MESSAGE_KEY_TOGGLE_SUFFIX);
 	int toggle_suffix = toggle_suffix_t->value->int32;
 	persist_write_int(MESSAGE_KEY_TOGGLE_SUFFIX, toggle_suffix);
-	
+
 // Update
 	layer_mark_dirty(s_background_layer); 	// update background
 	layer_mark_dirty(s_hands_layer); 		// update Hands
@@ -109,7 +100,7 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
 // Hour & Minute
 	graphics_context_set_fill_color(ctx, GColorWhite);
 	graphics_context_set_stroke_color(ctx, GColorBlack);
-	
+
 	gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((tick_time->tm_hour % 12) * 6) + (tick_time->tm_min / 10))) / (12 * 6)); // TRIG_MAX_ANGLE * 50 / 60);
 	gpath_draw_filled(ctx, s_hour_arrow);
 	gpath_draw_outline(ctx, s_hour_arrow);
@@ -122,11 +113,11 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
 static void date_update_proc(Layer *layer, GContext *ctx) {
 	time_t now = time(NULL);
 	struct tm *tick_time = localtime(&now);
-	
+
 	static char date_current[16];
 	strftime(date_current, 80, "%d%m", tick_time);
-	
-	int toggle_suffix = persist_read_int(MESSAGE_KEY_TOGGLE_SUFFIX);	
+
+	int toggle_suffix = persist_read_int(MESSAGE_KEY_TOGGLE_SUFFIX);
 
 // Top
 	int toggle_digital = persist_read_int(MESSAGE_KEY_TOGGLE_DIGITAL);
@@ -146,9 +137,9 @@ static void date_update_proc(Layer *layer, GContext *ctx) {
 	strcat(char_suffix,"%e");
 	if(toggle_suffix == 1) {
 		getSuffix(char_suffix, date_current);
-	}	
+	}
 	getMonth(char_suffix, 0, tick_time);
-	
+
 	strftime(s_month_buffer, sizeof(s_month_buffer), char_suffix, tick_time);
 	text_layer_set_text(s_month_label, s_month_buffer);
 }
@@ -174,17 +165,17 @@ static void window_load(Window *window) {
 	layer_add_child(window_layer, s_date_layer);
 
 // Locations
-	s_sharingan_layer = bitmap_layer_create(GRect(bounds.size.w/2 - 136/2, bounds.size.h/2 - 136/2, 136, 136));
-	s_hour_layer	= text_layer_create(GRect(                 0, bounds.size.h/2-16, bounds.size.w/2-15, 35));
- 	s_minute_layer	= text_layer_create(GRect(bounds.size.w/2+15, bounds.size.h/2-16, bounds.size.w/2-15, 35));
-	s_weekday_label = text_layer_create(GRect(0,                0, bounds.size.w, 25)); // Top
-	s_month_label	= text_layer_create(GRect(0, bounds.size.h-16, bounds.size.w, 16)); // Bottom
+	s_sharingan_layer = bitmap_layer_create(GRect(bounds.size.w / 2 - 136 / 2, bounds.size.h / 2 - 136 / 2, 136, 136));
+	s_hour_layer      = text_layer_create(GRect(                     0, bounds.size.h / 2 - 16, bounds.size.w / 2 - 15, 35));
+ 	s_minute_layer    = text_layer_create(GRect(bounds.size.w / 2 + 15, bounds.size.h / 2 - 16, bounds.size.w / 2 - 15, 35));
+	s_weekday_label   = text_layer_create(GRect(                     0,                      0,          bounds.size.w, 25)); // Top
+	s_month_label     = text_layer_create(GRect(                     0,     bounds.size.h - 16,          bounds.size.w, 16)); // Bottom
 
 // Sharingan
 	bitmap_layer_set_bitmap(s_sharingan_layer, gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SHARINGAN_FULL));
 	layer_mark_dirty(bitmap_layer_get_layer(s_sharingan_layer));
 	#if defined(PBL_COLOR)
-		bitmap_layer_set_compositing_mode(s_sharingan_layer, GCompOpSet);	
+		bitmap_layer_set_compositing_mode(s_sharingan_layer, GCompOpSet);
 	#endif
 	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_sharingan_layer));
 
@@ -194,7 +185,7 @@ static void window_load(Window *window) {
 	text_layer_set_font(s_weekday_label, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NARUTO_15)));
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weekday_label));
 	text_layer_set_text(s_weekday_label, s_weekday_buffer);
-	
+
 // Month
 	text_layer_set_text_alignment(s_month_label, GTextAlignmentCenter);
 	text_layer_set_background_color(s_month_label, GColorClear);
@@ -206,14 +197,14 @@ static void window_load(Window *window) {
 	s_hands_layer = layer_create(bounds);
 	layer_set_update_proc(s_hands_layer, hands_update_proc);
 	layer_add_child(window_layer, s_hands_layer);
-	
+
 	bluetooth_callback(connection_service_peek_pebble_app_connection());
 }
 
 static void window_unload(Window *window) {
 	layer_destroy(s_background_layer);
 	layer_destroy(s_hands_layer);
-	
+
 	text_layer_destroy(s_hour_layer);
 	text_layer_destroy(s_minute_layer);
 	text_layer_destroy(s_weekday_label);
@@ -227,21 +218,21 @@ static void init() {
 		.unload = window_unload,
 	});
 	window_stack_push(s_window, true);
-	
+
 	UnobstructedAreaHandlers handlers = {
 		.change = unobstructed_change,
 	};
 	unobstructed_area_service_subscribe(handlers, NULL);
-	
+
 	connection_service_subscribe((ConnectionHandlers) {
-  		.pebble_app_connection_handler = bluetooth_callback
+		.pebble_app_connection_handler = bluetooth_callback
 	});
-	
+
 	const int inbox_size = 128;
 	const int outbox_size = 128;
 	app_message_register_inbox_received(inbox_received_handler);
 	app_message_open(inbox_size, outbox_size);
-	
+
 	s_weekday_buffer[0] = '\0';
 	s_month_buffer[0] 	= '\0';
 
@@ -254,7 +245,7 @@ static void init() {
 	GPoint center = grect_center_point(&bounds);
 	gpath_move_to(s_minute_arrow, center);
 	gpath_move_to(s_hour_arrow, center);
-	
+
 	tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
 }
 
--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -1,7 +1,7 @@
 module.exports = [
-	{ 
-    	"type": "heading", 
-    	"defaultValue": "Sharingan",
+	{
+		"type": "heading",
+		"defaultValue": "Sharingan",
 		"size": 1,
 	},
 	{
@@ -35,57 +35,10 @@ module.exports = [
 				"defaultValue": false,
 			}
 		]
- 	},
-// 	{
-// 		"type": "section",
-// 		"items": [
-// 			{
-// 				"type": "heading",
-// 				"defaultValue": "Battery"
-// 			},
-// 			{
-// 				"type": "toggle",
-// 				"messageKey": "TOGGLE_BATTERY",
-// 				"label": "Show battery icon",
-// 				"defaultValue": false,
-// 			},
-// 			{
-// 				"type": "select",
-// 				"messageKey": "SELECT_BATTERY_PERCENT",
-// 				"defaultValue": "20",
-// 				"label": "Low battery percent",
-// 				"options": [
-// 					{ 
-// 						"label": "10%",
-// 						"value": "10" 
-// 					},
-// 					{ 
-// 						"label": "20%",
-// 						"value": "20" 
-// 					},
-// 					{ 
-// 						"label": "30%",
-// 						"value": "30" 
-// 					},
-// 					{ 
-// 						"label": "40%",
-// 						"value": "40" 
-// 					},
-// 					{ 
-// 						"label": "50%",
-// 						"value": "50" 
-// 					},
-// 					{ 
-// 						"label": "60%",
-// 						"value": "60" 
-// 					},
-// 				]
-// 			},
-// 		]
-//	},
+	},
 	{
- 		"type": "section",
- 		"items": [
+		"type": "section",
+		"items": [
 			{
 				"type": "heading",
 				"defaultValue": "Date"
@@ -101,6 +54,9 @@ module.exports = [
 	{
 		"type": "submit",
 		"defaultValue": "Save"
-	}
+	},
+	{
+		"type": "text",
+		"defaultValue": " Version 0.6",
+	},
 ];
-
